2291919d6c464f76850e2230b5bb4f9101d86dfc
Dyllinb1 <145726514+Dyllinb1@users.noreply.github.com>
2023-09-28 14:26:38 -0500

    Update main.py
    Added additional information from previous chapter 4.

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-for i in range(5):
-  print ('A number:', i)
+def main():
+  intro()
+  try: 
+    miles_needed = int(input('Enter the number of miles: '))
+    miles_to_kilometers(miles_needed)
+  except:
+    print('An exception occurred, try again entering only a number.')
+    print()
+    main()
+
+def intro():
+  print("This program converts measurements")
+  print('in miles to kilometers. For your')
+  print('reference the formula is: ')
+  print(' 1 mile = 1.609 kilometers')
+  print()
+
+def miles_to_kilometers(miles):
+  kilometers = miles * 1.609
+  print('That converts to', kilometers, 'kilometers.')
+
+main() 
